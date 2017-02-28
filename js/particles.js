a285989933ecdc9c61f099e79f13af7015252468
Cursi <razvan.diib@yahoo.com>
2017-02-28 21:10:12 +0200

    Create particles.js

---
+++ js/particles.js
@@ -0,0 +1,15 @@
+$(document).ready(function() 
+{
+  $('#form2').particleground
+  (
+  {
+    dotColor: 'rgba(255,250,250,0.1)',
+    lineColor: 'rgba(250,250,250,0.04)'
+  });
+  $('#form3').particleground
+  (
+  {
+    dotColor: 'rgba(36,46,58,0.15)',
+    lineColor: 'rgba(36,46,58,0.08)'
+  });
+});
